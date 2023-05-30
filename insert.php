08f35e8330c37eb61b16aede2dc3e0af13c7bea6
aluno <aluno@INTRAUECA.CEFET.COM>
2023-05-30 11:35:04 -0300

    Aula 1 - Formulários

---
+++ insert.php
@@ -0,0 +1,9 @@
+<?php
+$nome = $_GET['nome'];
+$telefone = $_GET['telefone'] ;
+$endereco = $_GET['endereco'] ;
+$cep = $_GET['cep'] ;
+
+echo("Olá $nome. Suas infos são: $telefone , $endereco , $cep .");
+
+?>
