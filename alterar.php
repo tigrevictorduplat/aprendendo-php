696d28361adf018b1968bf2186ba054328ddf751
aluno <aluno@INTRAUECA.CEFET.COM>
2023-06-27 11:43:27 -0300

    Delete finished

--- alterar.php
+++ alterar.php
@@ -1,25 +1,25 @@
 <?php 
-$id = $_GET['id'];
+$id = $_GET["id"];
 include("conexao.php");
 $result = mysqli_query($con, "SELECT * from cadastro WHERE ID = '$id' ");
 $row = mysqli_fetch_array ($result);
 ?>
 
-<form name="form" action="update.php?id=<?= $id ?>" method="$POST">
+<form name="form" action="update.php" method="$_GET">
     
     <label for="id">ID:</label>
-    <input type="text" name="id" placeholder="<?= $row["ID"]; ?>" readonly> <br>
+    <input type="text" name="id" value="<?= $row["ID"]; ?>" readonly> <br>
     <label for="name">Nome:</label>
-    <input type="text" name="nome" placeholder="<?= $row["Nome"]; ?>"> <br>
+    <input type="text" name="nome" value="<?= $row["Nome"]; ?>"> <br>
 
     <label for="telefone">Telefone:</label>
-    <input type="text" name="telefone" placeholder="<?= $row["Telefone"]; ?>"> <br>
+    <input type="text" name="telefone" value="<?= $row["Telefone"]; ?>"> <br>
 
     <label for="endereco">Endereço: </label>
-    <input type="text" name="endereco" placeholder="<?= $row["Endereco"]; ?>"> <br>
+    <input type="text" name="endereco" value="<?= $row["Endereco"]; ?>"> <br>
 
     <label for="cep">CEP:</label>
-    <input type="text" name="cep" placeholder="<?= $row["CEP"]; ?>">
+    <input type="text" name="cep" value="<?= $row["CEP"]; ?>">
     <br>
     <br>
     <input type="submit" value="SALVAR">
--- delete.php
+++ delete.php
@@ -11,6 +11,6 @@ $result = mysqli_query($con, "SELECT * from cadastro WHERE ID = '$id' ");
 $row = mysqli_fetch_array ($result);
 
 mysqli_query($con,
-"UPDATE cadastro SET Nome='$nome', Telefone='$telefone', Endereco='$endereco' , CEP='$cep' WHERE ID = '$id' ");
-echo("Olá $nome. Suas infos: $telefone , $endereco , $cep . Foram atualizadas com sucesso!");
+"DELETE FROM cadastro  WHERE ID = '$id' ");
+echo("Olá $nome. Suas infos: $telefone , $endereco , $cep . Foram excluidos com sucesso!");
 ?>
